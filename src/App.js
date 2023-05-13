f506ad898e478913f9215ff4eb53f248eb5ebf3e
Fan Yiwei <nf140901@gmail.com>
2023-05-13 16:07:55 +0800

    Added MyLocation component and style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import './App.css'
 
 import React from "react"
 import Map from './Components/Map';
+import Filter from './Components/Filter'
+import SearchBox from './Components/GoogleSearchBar';
 
 function App() {
   return (
     <div className='app' >
+      {/* google map component */}
       <Map />
+
+      {/* search bar component */}
+      <SearchBox />
+
+      {/* filter component */}
+      <Filter />
     </div>
   )
 }
