4a8d07a07e84ee9fc0971fc165785f07acb162cd
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-05-14 14:32:29 +0800

    Updated searchbar

--- src/Components/GoogleSearchBar/index.jsx
+++ src/Components/GoogleSearchBar/index.jsx
@@ -1,34 +1,57 @@
-import React, { useRef, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React, { useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import Grid from "@mui/material/Unstable_Grid2";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+
 const SearchBox = ({ placeholder, onPlacesChanged }) => {
-  const inputRef = useRef(null)
-  const searchBoxRef = useRef(null)
+  const inputRef = useRef(null);
+  const searchBoxRef = useRef(null);
 
   const handlePlacesChanged = () => {
     if (onPlacesChanged) {
-      onPlacesChanged(searchBoxRef.current.getPlaces())
+      onPlacesChanged(searchBoxRef.current.getPlaces());
     }
   };
 
-  useEffect(() => {
-    if (window.google) {
-      const input = ReactDOM.findDOMNode(inputRef.current)
-      searchBoxRef.current = new window.google.maps.places.SearchBox(input)
-      searchBoxRef.current.addListener('places_changed', handlePlacesChanged)
-
-      return () => {
-        // https://developers.google.com/maps/documentation/javascript/events#removing
-        window.google.maps.event.clearInstanceListeners(searchBoxRef.current)
-      }
-    }
-  }, [])
+  useEffect(() => {}, []);
 
   return (
-    <div className="search">
-      <input className='searchbox' ref={inputRef} placeholder={placeholder} type="text" />
-    </div>)
-}
+    <Grid
+      className="search"
+      container
+      spacing={0}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid xs={8}>
+        <TextField id="outlined-search" label="Search field" type="search" />
+      </Grid>
+      <Grid
+        xs={2}
+        className="border"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <SearchIcon fontSize="large" />
+      </Grid>
+      <Grid xs={2} justifyContent="center" alignItems="stretch">
+        <CloseIcon fontSize="large" />
+      </Grid>
+    </Grid>
+    // <div className="search">
+    //   <input
+    //     className="searchbox"
+    //     ref={inputRef}
+    //     placeholder={placeholder}
+    //     type="text"
+    //   />
+    // </div>
+  );
+};
 
 export default SearchBox;
-
