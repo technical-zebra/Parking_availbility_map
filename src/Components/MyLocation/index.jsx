28da8dccb78979f1fface746d96358d30075b53d
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-05-14 14:47:44 +0800

    Updated myLocation css

--- src/Components/MyLocation/index.jsx
+++ src/Components/MyLocation/index.jsx
@@ -1,14 +1,14 @@
-
-import React from 'react'
-import Fab from '@mui/material/Fab';
-import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import React from "react";
+import Fab from "@mui/material/Fab";
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
 import "./index.css";
 
-
 export default function MyLocationFAB() {
-    return (
-        <Fab className="FAB-container" aria-label="my_location">
-          <LocationSearchingIcon />
-        </Fab>
-    );
-  }
+  return (
+    <div className="container">
+      <Fab className="FAB" aria-label="my_location">
+        <LocationSearchingIcon />
+      </Fab>
+    </div>
+  );
+}
