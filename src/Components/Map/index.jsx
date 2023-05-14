b62fcf75144c348523b68f13928ac4262499b113
technical-zebra <ke.zhang3@my.jcu.edu.au>
2023-05-14 13:41:41 +0800

    Updated MapContainer default config

--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -15,7 +15,7 @@ export default function Map() {
   }
 
   return (
-      <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer className="map" center={[1.290, 103.851]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
