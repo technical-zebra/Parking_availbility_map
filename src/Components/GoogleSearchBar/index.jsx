8b5f4b26855ca067a510c8bbab832a8586c2ca45
Fan Yiwei <nf140901@gmail.com>
2023-05-20 00:27:55 +0800

    Added Search bar component

--- src/Components/GoogleSearchBar/index.jsx
+++ src/Components/GoogleSearchBar/index.jsx
@@ -1,57 +1,25 @@
-import React, { useRef, useEffect } from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 
-import Grid from "@mui/material/Unstable_Grid2";
-import TextField from "@mui/material/TextField";
-import SearchIcon from "@mui/icons-material/Search";
-import CloseIcon from "@mui/icons-material/Close";
+import { useMap } from 'react-leaflet/hooks';
+import { OpenStreetMapProvider, GeoSearchControl } from 'leaflet-geosearch';
 
-const SearchBox = ({ placeholder, onPlacesChanged }) => {
-  const inputRef = useRef(null);
-  const searchBoxRef = useRef(null);
+export default function SearchBox() {
+  const map = useMap();
 
-  const handlePlacesChanged = () => {
-    if (onPlacesChanged) {
-      onPlacesChanged(searchBoxRef.current.getPlaces());
-    }
-  };
+  const provider = new OpenStreetMapProvider({
+    params: {
+      email: 'terryzhangke@gmail.com', // auth for large number of requests
+    },
+  })
 
-  useEffect(() => {}, []);
+  const search = new GeoSearchControl({
+    provider: provider,
+    style: 'bar',
+  })
 
-  return (
-    <Grid
-      className="search"
-      container
-      spacing={0}
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Grid xs={8}>
-        <TextField id="outlined-search" label="Search field" type="search" />
-      </Grid>
-      <Grid
-        xs={2}
-        className="border"
-        justifyContent="center"
-        alignItems="stretch"
-      >
-        <SearchIcon fontSize="large" />
-      </Grid>
-      <Grid xs={2} justifyContent="center" alignItems="stretch">
-        <CloseIcon fontSize="large" />
-      </Grid>
-    </Grid>
-    // <div className="search">
-    //   <input
-    //     className="searchbox"
-    //     ref={inputRef}
-    //     placeholder={placeholder}
-    //     type="text"
-    //   />
-    // </div>
-  );
-};
+  map.addControl(search);
 
-export default SearchBox;
+  return null
+}
+
+
